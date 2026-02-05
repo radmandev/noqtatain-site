fd6e31d2fd0aafeedd99bda221b57834eb3d81f0
radmandev <emadradman.dev@gmail.com>
2026-02-05 19:24:44 +0300

    Add inquiry note to CTAs

--- src/pages.config.js
+++ src/pages.config.js
@@ -5,6 +5,8 @@ import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import hk24 from './pages/hk24';
+import finoo from './pages/finoo';
+import hamsa from './pages/hamsa';
 import Portal from './pages/Portal.jsx';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
@@ -16,6 +18,8 @@ export const PAGES = {
     Home,
     Services,
     hk24,
+    finoo,
+    hamsa,
     Portal,
     portal: Portal,
     PrivacyPolicy,
